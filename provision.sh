ddf17ecac0e2d95be0db5fe58b37aa39e76cd0f6
Gareth Smith <gsmith@pivotal.io>
2017-11-07 14:28:41 +0000

    Update for latest ubuntu and go
    Signed-off-by: Hannes Hoerl <hhoerl@pivotal.io>

--- provision.sh
+++ provision.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
-# Use us.archive.ubuntu.com instead of EC2 mirrors, which are broken
-sed -i -e 's/http.*\.archive\.ubuntu\.com/http:\/\/us.archive.ubuntu.com/' /etc/apt/sources.list
-
-rm -rf /etc/apt/sources.list.d/multiverse-trusty*
+set -eu
+set -o pipefail
 
 apt-get -y update
 apt-get -y clean
 
-apt-get install -y git vim-nox jq cgroup-lite
+apt-get install -y git vim-nox jq cgroup-lite build-essential
 
-wget -qO- https://storage.googleapis.com/golang/go1.4.2.linux-amd64.tar.gz | tar -C /usr/local -xzf -
+wget -qO- https://redirector.gvt1.com/edgedl/go/go1.9.2.linux-amd64.tar.gz | tar -C /usr/local -xzf -
 
 #Set up $GOPATH and add go executables to $PATH
 cat > /etc/profile.d/go_env.sh <<\EOF
@@ -26,7 +24,7 @@ pushd $GOPATH/src/github.com/opencontainers/
 git clone https://github.com/opencontainers/runc
 pushd runc
 
-GOPATH=$PWD/Godeps/_workspace:$GOPATH go build -o runc .
+go build -o runc .
 cp runc /usr/local/bin/runc
 
 cp /vagrant/mount-tmpfs.sh /etc/init.d/
